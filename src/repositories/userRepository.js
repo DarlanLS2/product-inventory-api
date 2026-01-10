880d73e8a68537785b1778743f6cdcc1cb75eece
DarlanLS2 <Darlanleite.arnaldo@gmail.com>
2026-01-10 09:41:16 -0300

    feat(repositories): add userRepository.js

---
+++ src/repositories/userRepository.js
@@ -0,0 +1,46 @@
+import { User } from "../entities/User";
+
+export class UserRepository {
+  constructor(UserModel) {
+    this.userModel = UserModel
+  }
+
+  async getByEmail(email) {
+    try{
+      const user = await this.userModel.findOne({ where: {email: email}});
+
+      return new User({
+        id: user.id,
+        email: user.email,
+        passWordHash: user.passWordHash
+      });
+    } catch {
+      throw new Error("Erro ao acessar o banco");
+    }
+  }
+  
+  async register(user) {
+    try {
+      const createdUser = await this.userModel.create({
+        email: user.email,
+        passWordHash: user.passWordHash
+      });
+
+      return new user({
+        id: createdUser.id,
+        email: createdUser.email,
+        passWordHash: createdUser.passWordHash
+      })
+    } catch {
+      throw new Error("Erro ao acessar o banco");
+    }
+  }
+
+  async delete(id) {
+    try {
+      return await this.userModel.destroy({where: {id: id}})
+    } catch {
+      throw new Error("Erro ao acessar o banco")
+    }
+  }
+}
