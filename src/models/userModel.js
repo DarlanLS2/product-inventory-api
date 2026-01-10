1b93072dc29295bd33c6582525063089c591e483
DarlanLS2 <Darlanleite.arnaldo@gmail.com>
2026-01-10 09:50:48 -0300

    feat: criar userModel.js

---
+++ src/models/userModel.js
@@ -0,0 +1,14 @@
+import { DataTypes } from "sequelize";
+import { sequelize } from "../database/sequelize.js";
+
+export const User = sequelize.define("user", {
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true
+  },
+  passWordHash: {
+    type: DataTypes.STRING,
+    allowNull: false
+  }
+});
