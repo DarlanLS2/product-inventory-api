639a095eb58e0c2b6702f0e1935d6350fe0ddf98
DarlanLS2 <Darlanleite.arnaldo@gmail.com>
2026-02-01 10:20:35 -0300

    refactor: decouple app initialization from server startup
    Move database sync, models, repositories, controllers and routes wiring
    out of app.js into server.js. App initialization is now handled by an
    async factory function with dependency injection, improving separation
    of concerns and testability.

--- app.js
+++ app.js
@@ -1,40 +1,34 @@
 import dotenv from "dotenv";
 import express from "express";
-import path from "path";
 import cors from "cors";
-import { syncDatabase } from "./src/database/sync.js";
-import { User } from "./src/database/models/userModel.js";
-import { UserRepository } from "./src/repositories/userRepository.js";
-import { UserController } from "./src/controllers/userController.js";
-import { UserService } from "./src/services/userService.js";
-import { UserRoute } from "./src/routes/userRoute.js"
 
-import { authMidleware } from "./src/authentication/authMidleware.js";
+export default async function(
+  database,
+  models,
+  repositories,
+  controllers,
+  services,
+  routes,
+  middlewares
+) {
+  dotenv.config();
 
-import { Product } from "./src/database/models/productModel.js"
-import { ProductRepository } from "./src/repositories/productRepository.js";
-import { ProductController } from "./src/controllers/productController.js";
-import { ProductRoute } from "./src/routes/productRoute.js";
+  const app = express(); 
+  app.use(cors());
+  app.use(express.json());
 
-dotenv.config();
+  await database.syncDatabase();
 
-const __dirname = path.resolve(); 
-const app = express(); 
+  const userRepository = new repositories.UserRepository(models.User);
+  const userService = new services.UserService(userRepository);
+  const userController = new controllers.UserController(userService);
+  const userRoutes = new routes.UserRoute(app, middlewares.authMidleware, userController);
+  userRoutes.create();
 
-app.use(cors());
-app.use(express.json());
+  const productRepository = new repositories.ProductRepository(models.Product)
+  const productController = new controllers.ProductController(productRepository)
+  const productRoutes = new routes.ProductRoute(app, middlewares.authMidleware, productController)
+  productRoutes.create()
 
-await syncDatabase();
-
-const userRepository = new UserRepository(User);
-const userService = new UserService(userRepository);
-const userController = new UserController(userService);
-const userRoutes = new UserRoute(app, authMidleware, userController);
-userRoutes.create();
-
-const productRepository = new ProductRepository(Product)
-const productController = new ProductController(productRepository)
-const productRoutes = new ProductRoute(app, authMidleware, productController)
-productRoutes.create()
-
-export default app;
+  return app;
+}
