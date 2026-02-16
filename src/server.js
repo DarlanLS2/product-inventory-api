aa70403b1a3e3a9c590de180948e5ccda077a845
DarlanLS2 <Darlanleite.arnaldo@gmail.com>
2026-02-16 14:25:53 -0300

    fix(server): incorrect import paths

---
+++ src/server.js
@@ -0,0 +1,35 @@
+import { syncDatabase } from "./database/sync.js";
+import { User } from "./database/models/userModel.js";
+import { UserRepository } from "./repositories/userRepository.js";
+import { UserController } from "./controllers/userController.js";
+import { UserService } from "./services/userService.js";
+import { UserRoute } from "./routes/userRoute.js"
+
+import { authMidleware } from "./authentication/authMidleware.js";
+
+import { Product } from "./database/models/productModel.js"
+import { ProductRepository } from "./repositories/productRepository.js";
+import { ProductController } from "./controllers/productController.js";
+import { ProductRoute } from "./routes/productRoute.js";
+
+import init from "./app.js"
+
+const app = await init(
+  { syncDatabase },
+  { User, Product },
+  { UserRepository, ProductRepository },
+  { UserController, ProductController },
+  { UserService },
+  { UserRoute, ProductRoute },
+  { authMidleware },
+)
+
+app.listen(process.env.PORT, "0.0.0.0", () => {
+  console.log(`
+    ------------------------\n
+    PORTA: ${process.env.PORT}\n
+    ------------------------\n
+    Conexão com o app: ok\n
+    ------------------------\n
+    `);
+});
